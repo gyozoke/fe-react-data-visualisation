c5928d2ff48afa62e9c456f0075ca5d89cca70ea
grace-hartley <gracerita.hartley@gmail.com>
2024-01-29 16:29:44 +0000

    formatted data in table

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   return (
     <>
       <Header />
-      <MeteorSearch searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      {/* <MeteorSearch searchTerm={searchTerm} setSearchTerm={setSearchTerm} /> */}
       <MeteorList meteors={meteors} />
     </>
   );
--- src/components/MeteorList.jsx
+++ src/components/MeteorList.jsx
@@ -1,25 +1,33 @@
-import MeteorItem from "./MeteorItem";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import "../MeteorList.css";
 
 export default function MeteorList(props) {
   const { meteors } = props;
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "name", headerName: "Name", width: 130 },
+    { field: "mass", headerName: "Mass (g)", width: 130 },
+    { field: "fall", headerName: "Fall", width: 130 },
+    { field: "year", headerName: "Year", width: 130 },
+    // { field: "geolocation", headerName: "GeoLocation", width: 130 },
+  ];
   return (
     <section>
       <h2>Meteor List</h2>
-      <ul>
-        {meteors.map((meteor) => {
-          const { name, id, mass, fall, year } = meteor;
-          return (
-            <MeteorItem
-              key={`${name}${id}${mass}${fall}${year}`}
-              name={name}
-              id={id}
-              mass={mass}
-              fall={fall}
-              year={year}
-            />
-          );
-        })}
-      </ul>
+      <div id="data-table" style={{ height: "100%", width: "100%" }}>
+        <DataGrid
+          rows={meteors}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 10 },
+            },
+          }}
+          slots={{ toolbar: GridToolbar }}
+          pageSizeOptions={[10, 20, 50, 100]}
+          checkboxSelection
+        />
+      </div>
     </section>
   );
 }
